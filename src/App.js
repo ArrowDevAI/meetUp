ea7ca52f4f07fddbc0a9f4240fdcc9c0ae507ed7
Trevor Higgins <higginst.th@gmail.com>
2024-09-24 18:45:31 -0400

    edited clear button in NOE test

--- src/App.js
+++ src/App.js
@@ -60,7 +60,8 @@ const App = () => {
         {infoAlert.length ? <InfoAlert text = {infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text = {errorAlert}/> : null}
         {warningAlert.length? <WarningAlert text = {warningAlert} /> : null}
-      </div>      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert = {setInfoAlert} />
+      </div>     
+       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert = {setInfoAlert} />
       <NumberOfEvents currentNOE = {currentNOE} setCurrentNOE = {setCurrentNOE} setErrorAlert = {setErrorAlert} />
       <div className = "charts-container">
       <CityEventsChart allLocations={allLocations} events={events} />
